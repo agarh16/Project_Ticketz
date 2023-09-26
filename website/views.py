a0381b8a6a6aba7247c9f6396455633ef3e17f53
agarh16 <ma.garcia.hdz@gmail.com>
2023-09-26 12:15:24 +0200

    updates to routes

--- website/views.py
+++ website/views.py
@@ -1,9 +1,9 @@
 # Store all the main views or the URL endpoints 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
 

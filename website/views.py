54229d2389a5b023db7a379d33ed3fc9df57af58
agarh16 <ma.garcia.hdz@gmail.com>
2023-09-27 22:37:13 +0200

    changes

--- website/views.py
+++ website/views.py
@@ -5,8 +5,13 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-   
+    # if user is client 
     return render_template("home_client.html")
+
+    #if user is employee
+    #return render_template("home_employee.html")
+
+
 
 
     

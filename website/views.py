22d246691536903faef496277e4a28a5486fb98f
agarh16 <ma.garcia.hdz@gmail.com>
2023-10-03 15:18:43 +0200

    test database added

--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 # Store all the main views or the URL endpoints 
 from flask import Blueprint, render_template, request, session
 from flask_login import login_required, current_user
+from . import db
+from .models import User
 
 views = Blueprint('views', __name__)
 
@@ -12,6 +14,14 @@ def home():
 
     #if user is employee
     #return render_template("home_employee.html")
+
+@views.route('/test-database')
+def all_users_table():
+    results = db.session.query(User).all()
+    for result in results:
+        printed_results = result.firstName, result.lastName, result.email, result.isEmployee
+        print(printed_results)
+    return render_template("test.html")
 
 
 
